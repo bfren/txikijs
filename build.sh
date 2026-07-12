32b256e8fedc7c94247255890a5e04096197e9d3
bfren <bf@bfren.dev>
2026-07-12 17:05:37 +0100

    Minifying build

--- build.sh
+++ build.sh
@@ -4,7 +4,14 @@ set -eu
 
 # build 
 cd txiki.js
-make
+cmake -B build-slim \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_WITH_WASM=OFF \
+    -DBUILD_WITH_SQLITE=OFF \
+    -DBUILD_WITH_LTO=ON \
+    -DBUILD_WITH_GC_SECTIONS=ON \
+    -DBUILD_WITH_STRIP=ON
+cmake --build build-slim
 
 # move binary to root
 mv ./build/tjs /
