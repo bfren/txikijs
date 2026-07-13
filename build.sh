862912a4d5a6398b6578b7885d1c08cacc7196db
bfren <bf@bfren.dev>
2026-07-13 09:53:50 +0100

    Disabling LTO

--- build.sh
+++ build.sh
@@ -8,11 +8,10 @@ cmake -B build-slim \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_WITH_WASM=OFF \
     -DBUILD_WITH_SQLITE=OFF \
-    -DBUILD_WITH_LTO=ON \
     -DBUILD_WITH_GC_SECTIONS=ON \
     -DBUILD_WITH_STRIP=ON \
-    -DCMAKE_EXE_LINKER_FLAGS="-static" \
-    -DFFI_LIB=/usr/lib/libffi.a
+    -DBUILD_WITH_FFI=OFF \
+    -DCMAKE_EXE_LINKER_FLAGS="-static"
 cmake --build build-slim
 
 # move binary to root
