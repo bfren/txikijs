897670d634337f89a1f5cb325dbdad2b2ed8b05d
bfren <bf@bfren.dev>
2026-07-13 09:54:07 +0100

    No need to patch FFI - building without it

--- download.sh
+++ download.sh
@@ -14,9 +14,3 @@ git clone \
     --shallow-submodules
 
 # apply patches
-
-# the correct, portable format specifier for size_t in C is %zu, not %lu
-# fixes the following build error:
-# format '%lu' expects argument of type 'long unsigned int', but argument 3 has type 'size_t' {aka 'unsigned int'} [-Werror=format=]
-sed -i 's/%lu", typesz/%zu", typesz/' txiki.js/src/mod_ffi.c
-
